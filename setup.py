5c5f1e05f2ec34dc8408ece6b102f6515b6e1786
Samuel Scherer <github@samuelscherer.de>
2018-04-11 16:17:11 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='python-tuya',
+	version='0.1',
+	description='Python implementation of the Tuya protocol',
+	author='clach04',
+	url='https://github.com/clach04/python-tuya',
+	packages=['pycrypto'],
+	)
