fb62d97111c0b5c93e9027eab02d49f6156843cb
Samuel Scherer <github@samuelscherer.de>
2018-04-11 18:34:23 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from distutils.core import setup
 
 setup(name='python-tuya',
 	version='0.1',
+        packages=['pytuya'],
 	description='Python implementation of the Tuya protocol',
 	author='clach04',
 	url='https://github.com/clach04/python-tuya',
