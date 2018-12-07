ed3b82c3d6ea7205ed702c06c866c0742a2c3def
dominik dienlin <dominik@dienlin.net>
2018-12-07 19:03:37 +0100

    removed cli import from pytuya.__init__ (pytuya doesn't depend on cli)

--- pytuya/__init__.py
+++ pytuya/__init__.py
@@ -1,7 +1,8 @@
 from .devices import XenonDevice, Device, BulbDevice, OutletDevice, CoverDevice, SET
 from .utils import query_devices, KeyExtractor
-from pytuya.cli import cli_root
 
 version_tuple = (7, 0, 4)
 version = version_string = __version__ = '%d.%d.%d' % version_tuple
 __author__ = 'clach04'
+
+
